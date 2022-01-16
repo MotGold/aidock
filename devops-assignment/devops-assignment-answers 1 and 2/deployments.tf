451f6fee3b3a4fafb25163ddeb418ae9383f3154
MotGold <72747315+MotGold@users.noreply.github.com>
2022-01-16 16:16:19 +0200

    Add files via upload

---
+++ devops-assignment/devops-assignment-answers 1 and 2/deployments.tf
@@ -0,0 +1,178 @@
+#############################################################################
+### Deployment manifests for 3 applications (frontend, backend, database) ###
+### feel free to change anything and to implement any function or method  ###
+#############################################################################
+
+resource "kubernetes_deployment" "instance" {
+  for_each = {for inst in var.apps_info:  inst.app_name => inst}
+  metadata {
+    name      = "${each.value.app_name}"
+    namespace = "${each.value.app_name}"
+    labels = {
+      name = "${each.value.name_label}"
+      tier = "${each.value.tier_label}"
+    }
+  }
+  spec {
+    selector {
+      match_labels = {
+        name = "${each.value.name_label}"
+        tier = "${each.value.tier_label}"
+      }
+    }
+    template {
+      metadata {
+        name = "${each.value.app_name}"
+        labels = {
+          name = "${each.value.name_label}"
+          tier = "${each.value.tier_label}"
+        }
+      }
+      spec {
+        container {
+          name  = "${each.value.app_name}"
+          image = "nginx"
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_deployment" "database" {
+  metadata {
+    name      = var.database.db.app_name
+    namespace = var.database.db.app_name
+    labels = {
+      name = var.database.db.name_label
+      tier = var.database.db.tier_label
+    }
+  }
+  spec {
+    selector {
+      match_labels = {
+        name = var.database.db.name_label
+        tier = var.database.db.tier_label
+      }
+    }
+    template {
+      metadata {
+        name = var.database.db.app_name
+        labels = {
+          name = var.database.db.name_label
+          tier = var.database.db.tier_label
+        }
+      }
+      spec {
+        container {
+          name  = var.database.db.app_name
+          image = "mongo"
+        }
+      }
+    }
+  }
+}
+
+/*
+resource "kubernetes_deployment" "app1" {
+  metadata {
+    name      = var.app1_name
+    namespace = var.app1_name
+    labels = {
+      name = var.app1_labels.name
+      tier = var.app1_labels.tier
+    }
+  }
+  spec {
+    selector {
+      match_labels = {
+        name = var.app1_labels.name
+        tier = var.app1_labels.tier
+      }
+    }
+    template {
+      metadata {
+        name = var.app1_name
+        labels = {
+          name = var.app1_labels.name
+          tier = var.app1_labels.tier
+        }
+      }
+      spec {
+        container {
+          name  = var.app1_name
+          image = "nginx"
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_deployment" "app2" {
+  metadata {
+    name      = var.app2_name
+    namespace = var.app2_name
+    labels = {
+      name = var.app2_labels.name
+      tier = var.app2_labels.tier
+    }
+  }
+  spec {
+    selector {
+      match_labels = {
+        name = var.app2_labels.name
+        tier = var.app2_labels.tier
+      }
+    }
+    template {
+      metadata {
+        name = var.app2_name
+        labels = {
+          name = var.app2_labels.name
+          tier = var.app2_labels.tier
+        }
+      }
+      spec {
+        container {
+          name  = var.app2_name
+          image = "nginx"
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_deployment" "app3" {
+  metadata {
+    name      = var.app3_name
+    namespace = var.app3_name
+    labels = {
+      name = var.app3_labels.name
+      tier = var.app3_labels.tier
+    }
+  }
+  spec {
+    selector {
+      match_labels = {
+        name = var.app3_labels.name
+        tier = var.app3_labels.tier
+      }
+    }
+    template {
+      metadata {
+        name = var.app3_name
+        labels = {
+          name = var.app3_labels.name
+          tier = var.app3_labels.tier
+        }
+      }
+      spec {
+        container {
+          name  = var.app3_name
+          image = "mongo"
+        }
+      }
+    }
+  }
+}
+
+*/
